400ed6c6121be31eea9d50d3d1cc15b741e7c081
seolman <tjfehdgns@gmail.com>
2025-12-17 04:01:48 +0000

    add prometheus container

--- src/configs/prometheus.ts
+++ src/configs/prometheus.ts
@@ -2,4 +2,8 @@ import { collectDefaultMetrics, Registry } from "prom-client";
 
 export const register = new Registry();
 
+register.setDefaultLabels({
+  appName: "manga-app",
+});
+
 collectDefaultMetrics({ register });
