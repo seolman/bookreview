7a48a1cfc21d0acbcad6fe9feaf142a59b9766ab
seolman <tjfehdgns@gmail.com>
2025-12-17 04:39:46 +0000

    fix prometheus config

--- src/configs/prometheus.ts
+++ src/configs/prometheus.ts
@@ -2,8 +2,4 @@ import { collectDefaultMetrics, Registry } from "prom-client";
 
 export const register = new Registry();
 
-register.setDefaultLabels({
-  appName: "manga-app",
-});
-
 collectDefaultMetrics({ register });
